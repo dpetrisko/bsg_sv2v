7de034d137296af76c342f15b84d1bf8c60fd6f0
Dan Petrisko <petrisko@cs.washington.edu>
2025-08-03 16:28:22 -0700

    Update run_dc.tcl

--- scripts/tcl/run_dc.tcl
+++ scripts/tcl/run_dc.tcl
@@ -12,7 +12,6 @@ set DESIGN_FILELIST  $::env(DESIGN_FILELIST)   ;# Filelist path
 set DESIGN_ELAB_NAME $::env(DESIGN_ELAB_NAME)  ;# Design name to elaborate
 set OUTPUT_DIR       $::env(OUTPUT_DIR)        ;# Output directory
 set OUTPUT_FILE      $::env(OUTPUT_ELAB_FILE)  ;# Output filename
-set SETUP_FILE       $::env(SETUP_FILE)        ;# Extra design options
 
 ### Application setup
 
